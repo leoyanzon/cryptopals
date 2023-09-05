cde3ccdd2292949dd4881495434887e4f5f7e043
leoyanzon <leoyanzon@gmail.com>
2023-09-05 09:29:23 -0300

    challenge 1

---
+++ challenge 1.py
@@ -0,0 +1,10 @@
+import base64
+
+def hex_to_base64(input):
+    input_bytes = bytes.fromhex(input)
+    output_base64 = base64.b64encode(input_bytes).decode()
+    return output_base64
+
+hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+print(hex_to_base64(hex_string))
